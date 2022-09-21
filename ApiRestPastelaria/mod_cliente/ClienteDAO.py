ed60a83208cc469554f588aa6272f6075572eb06
rodrigolorenzetti <54286223+rodrigolorenzetti@users.noreply.github.com>
2022-09-21 13:36:31 -0300

    Model e DAO de cliente, produto e funcionário

---
+++ ApiRestPastelaria/mod_cliente/ClienteDAO.py
@@ -0,0 +1,92 @@
+# import da persistência
+import db
+from mod_cliente.ClienteModel import ClienteDB
+
+from fastapi import APIRouter
+from pydantic import BaseModel
+
+class Cliente(BaseModel):
+    codigo: int = None
+    nome: str
+    cpf: str
+    telefone: str = None
+    compra_fiado: int
+    dia_fiado: int
+    senha: str = None
+
+router = APIRouter()
+# Criar os endpoints de Cliente: GET, POST, PUT, DELETE
+
+@router.get("/cliente/", tags=["cliente"])
+def get_cliente():
+    try:
+        session = db.Session()
+        # busca todos
+        dados = session.query(ClienteDB).all()
+        return dados, 200
+    except Exception as e:
+        return {"msg": "Erro ao listar", "erro": str(e)}, 404
+    finally:
+        session.close()
+
+@router.get("/cliente/{id}", tags=["cliente"])
+def get_cliente(id: int):
+    try:
+        session = db.Session()
+        # busca um com filtro
+        dados = session.query(ClienteDB).filter(ClienteDB.id_cliente == id).all()
+        return dados, 200
+    except Exception as e:
+        return {"msg": "Erro ao listar", "erro": str(e)}, 404
+    finally:
+        session.close()
+
+@router.post("/cliente/", tags=["cliente"])
+def post_cliente(corpo: Cliente):
+    try:
+        session = db.Session()
+        dados = ClienteDB(None, corpo.nome,
+        corpo.cpf, corpo.telefone, corpo.compra_fiado, corpo.senha)
+        session.add(dados)
+        session.commit()
+        return {"msg": "Cadastrado com sucesso!", "id": dados.id_cliente}, 200
+    except Exception as e:
+        session.rollback()
+        return {"msg": "Erro ao cadastrar", "erro": str(e)}, 406
+    finally:
+        session.close()
+
+@router.put("/cliente/{id}", tags=["cliente"])
+def put_cliente(id: int, corpo: Cliente):
+    try:
+        session = db.Session()
+        dados = session.query(ClienteDB).filter(
+            ClienteDB.id_cliente == id).one()
+
+        dados.nome = corpo.nome
+        dados.cpf = corpo.cpf
+        dados.telefone = corpo.telefone
+        dados.senha = corpo.senha
+        dados.compra_fiado = corpo.compra_fiado
+        session.add(dados)
+        session.commit()
+        return {"msg": "Editado com sucesso!", "id": dados.id_cliente}, 201
+    except Exception as e:
+        session.rollback()
+        return {"msg": "Erro ao editar", "erro": str(e)}, 406
+    finally:
+        session.close()
+
+@router.delete("/cliente/{id}", tags=["cliente"])
+def delete_cliente(id: int):
+    try:
+        session = db.Session()
+        dados = session.query(ClienteDB).filter(ClienteDB.id_cliente == id).one()
+        session.delete(dados)
+        session.commit()
+        return {"msg": "Excluido com sucesso!", "id": dados.id_cliente}, 201
+    except Exception as e:
+        session.rollback()
+        return {"msg": "Erro ao excluir", "erro": str(e)}, 406
+    finally:
+        session.close()
